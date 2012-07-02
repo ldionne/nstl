994e4d8e3446888866e912f29401368fa050f05c
Louis Dionne <louis.dionne92@gmail.com>
2012-07-02 16:20:01 -0400

    Add a config file to turn on/off options.

---
+++ include/nstl/internal/config.h
@@ -0,0 +1,21 @@
+/*!
+ * Internal configuration file to enable/disable features depending on the
+ * platform.
+ *
+ * @author Louis Dionne
+ */
+
+#ifndef NSTL_INTERNAL_CONFIG_H
+#define NSTL_INTERNAL_CONFIG_H
+
+/*!
+ * This switch is enabled when it is allowed to pass empty arguments to macros.
+ */
+#define NSTL_CONFIG_EMPTY_MACRO_ARGS 0
+
+/*!
+ * This switch is enabled when variadic macros are supported.
+ */
+#define NSTL_CONFIG_VARIADIC_MACROS 0
+
+#endif /* !NSTL_INTERNAL_CONFIG_H */
