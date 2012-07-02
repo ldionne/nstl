9ccf8370cc399ba8ff34b6af9d28a76ca8cca02f
Louis Dionne <louis.dionne92@gmail.com>
2012-07-02 16:20:27 -0400

    Fix a bug when compiling in c89 where the NSTL_TYPE macro used an empty argument.

--- include/nstl/type.h
+++ include/nstl/type.h
@@ -9,6 +9,7 @@
 
 #include <nstl/internal/type.h>
 #include <nstl/internal/token.h>
+#include <nstl/internal/config.h>
 
 #include <chaos/preprocessor/recursion/expr.h>
 #include <chaos/preprocessor/seq/fold_left.h>
@@ -39,9 +40,22 @@
     CHAOS_PP_EXPR_S(s)(CHAOS_PP_SEQ_FOLD_LEFT_S(s,                             \
         NSTL_I_EXECUTE_STATEMENT,                                              \
         instructions,                                                          \
-        /* empty initial state */                                              \
+        NSTL_I_INITIAL_TYPE_STATE()                                            \
     ))                                                                         \
 /**/
+
+/*!
+ * This is necessary because we can not have an empty argument as the initial
+ * state in c89.
+ */
+#if NSTL_CONFIG_EMPTY_MACRO_ARGS
+#   define NSTL_I_INITIAL_TYPE_STATE()
+#else
+#   define NSTL_I_INITIAL_TYPE_STATE() \
+        ((NSTL_I_C89_COMPAT_0xDUMMY_MEMBER, NSTL_I_C89_COMPAT_0xDUMMY_MEMBER))
+#   define NSTL_TOKEN_NSTL_I_C89_COMPAT_0xDUMMY_MEMBER \
+        (N S T L _ I _ C 8 9 _ C O M P A T _ 0 X D U M M Y _ M E M B E R)
+#endif
 
 /*!
  * Execute a statement of the form ``instruction args...''.
@@ -65,7 +79,15 @@
  */
 #define NSTL_IMPLEMENT(self) NSTL_IMPLEMENT_S(CHAOS_PP_STATE(), self)
 
-#define NSTL_IMPLEMENT_S(s, self) NSTL_DUMPF_S(s, self)
+#if NSTL_CONFIG_EMPTY_MACRO_ARGS
+#   define NSTL_IMPLEMENT_S(s, self) NSTL_DUMPF_S(s, self)
+#else
+#   define NSTL_IMPLEMENT_S(s, self) \
+        NSTL_DUMPF_S(s, \
+            NSTL_UNSETF_S(s, self, NSTL_I_C89_COMPAT_0xDUMMY_MEMBER) \
+        ) \
+    /**/
+#endif
 
 /*!
  * Instruction used to remove a field from a nstl type.
