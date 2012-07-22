0864c62e3c052bec573c580098dd7f6b34d5c7c9
Louis Dionne <louis.dionne92@gmail.com>
2012-07-22 18:22:04 -0400

    Add the adjacent_find_cmp algorithm.

--- include/nstl/algorithm/adjacent_find.h
+++ include/nstl/algorithm/adjacent_find.h
@@ -11,7 +11,7 @@
 
 
 #define NSTL_ADJACENT_FIND(FwdIter, ValueType)                                 \
-NSTL_TYPE(nstl_adjacent_find(FwdIter, ValueType),                              \
+NSTL_TYPE(nstl_adjacent_find(FwdIter),                                         \
                                                                                \
 (defun adjacent_find                                                           \
 /**                                                                            \
@@ -22,7 +22,7 @@ NSTL_TYPE(nstl_adjacent_find(FwdIter, ValueType),                              \
  * The comparison between the consecutive elements is performed by applying    \
  * the @em nstl_eq operator.                                                   \
  */                                                                            \
-static NSTL_INLINE FwdIter nstl_adjacent_find(FwdIter, ValueType)              \
+static NSTL_INLINE FwdIter nstl_adjacent_find(FwdIter)                         \
                                             (FwdIter first, FwdIter last) {    \
     FwdIter next;                                                              \
     if (nstl_eq(FwdIter, FwdIter)(first, last))                                \
@@ -30,11 +30,48 @@ static NSTL_INLINE FwdIter nstl_adjacent_find(FwdIter, ValueType)              \
     nstl_copy_ctor(FwdIter)(&next, first);                                     \
     while (nstl_ne(FwdIter, FwdIter)(nstl_inc(FwdIter)(&next), last)) {        \
         if (nstl_eq(ValueType, ValueType)(nstl_deref(FwdIter)(first),          \
-                                          nstl_deref(FwdIter)(next)))          \
+                                          nstl_deref(FwdIter)(next))) {        \
+            nstl_dtor(FwdIter)(&next);                                         \
             return first;                                                      \
+        }                                                                      \
                                                                                \
         nstl_asg(FwdIter, FwdIter)(&first, next);                              \
     }                                                                          \
+    nstl_dtor(FwdIter)(&next);                                                 \
+    return last;                                                               \
+}                                                                              \
+)                                                                              \
+                                                                               \
+)                                                                              \
+/**/
+
+#define NSTL_ADJACENT_FIND_CMP(FwdIter, ValueType, Compare)                    \
+NSTL_TYPE(nstl_adjacent_find_cmp(FwdIter, Compare),                            \
+                                                                               \
+(defun adjacent_find_cmp                                                       \
+/**                                                                            \
+ * Search the range [@p first, @p last) for the first occurrence of two        \
+ * consecutive equal elements, and return an iterator to the first of these    \
+ * two elements.                                                               \
+ *                                                                             \
+ * The comparison between the consecutive elements is performed by applying    \
+ * the @p comp comparator.                                                     \
+ */                                                                            \
+static NSTL_INLINE FwdIter nstl_adjacent_find_cmp(FwdIter, Compare)            \
+                                (FwdIter first, FwdIter last, Compare comp) {  \
+    FwdIter next;                                                              \
+    if (nstl_eq(FwdIter, FwdIter)(first, last))                                \
+        return last;                                                           \
+    nstl_copy_ctor(FwdIter)(&next, first);                                     \
+    while (nstl_ne(FwdIter, FwdIter)(nstl_inc(FwdIter)(&next), last)) {        \
+        if (comp(nstl_deref(FwdIter)(first), nstl_deref(FwdIter)(next))) {     \
+            nstl_dtor(FwdIter)(&next);                                         \
+            return first;                                                      \
+        }                                                                      \
+                                                                               \
+        nstl_asg(FwdIter, FwdIter)(&first, next);                              \
+    }                                                                          \
+    nstl_dtor(FwdIter)(&next);                                                 \
     return last;                                                               \
 }                                                                              \
 )                                                                              \
@@ -46,7 +83,8 @@ static NSTL_INLINE FwdIter nstl_adjacent_find(FwdIter, ValueType)              \
 
 import nstl
 nstl.generate(cog,
-    'adjacent_find(FwdIter, ValueType)',
+    'adjacent_find(FwdIter)',
+    'adjacent_find_cmp(FwdIter, Compare)',
 
     token=True, mangle=True,
 )
@@ -54,7 +92,9 @@ nstl.generate(cog,
 ]]] */
 #include <joy/cat.h>
 #define NSTL_TOKEN_adjacent_find (a d j a c e n t _ f i n d)
-#define nstl_adjacent_find(FwdIter,  ValueType) JOY_CAT5(nstl_mangled_adjacent_find, _, FwdIter, _,  ValueType)
+#define nstl_adjacent_find(FwdIter) JOY_CAT3(nstl_mangled_adjacent_find, _, FwdIter)
+#define NSTL_TOKEN_adjacent_find_cmp (a d j a c e n t _ f i n d _ c m p)
+#define nstl_adjacent_find_cmp(FwdIter,  Compare) JOY_CAT5(nstl_mangled_adjacent_find_cmp, _, FwdIter, _,  Compare)
 /* [[[end]]] */
 
 #endif /* !NSTL_ALGORITHM_ADJACENT_FIND_H */
--- test/algorithm/adjacent_find.c
+++ test/algorithm/adjacent_find.c
@@ -9,34 +9,51 @@
 #include <seatest.h>
 
 
-NSTL_INSTANTIATE(NSTL_ADJACENT_FIND(nstl_pint, nstl_int))
+typedef nstl_bool (*Compare)(nstl_int x, nstl_int y);
 
-#define adjacent_find nstl_adjacent_find(nstl_pint, nstl_int)
+NSTL_INSTANTIATE(NSTL_ADJACENT_FIND(nstl_pint, nstl_int))
+NSTL_INSTANTIATE(NSTL_ADJACENT_FIND_CMP(nstl_pint, nstl_int, Compare))
+
+#define adjacent_find nstl_adjacent_find(nstl_pint)
+#define adjacent_find_cmp(first, last)                                         \
+    nstl_adjacent_find_cmp(nstl_pint, Compare)(                                \
+        first, last, nstl_eq(nstl_int, nstl_int)                               \
+    )                                                                          \
+/**/
 
 static void test_should_not_find_any_when_none_equal_and_adjacent(void) {
     nstl_int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     nstl_pint found = adjacent_find(array, array + 10);
+    nstl_pint found_cmp = adjacent_find_cmp(array, array + 10);
+    assert_true(found == found_cmp);
     assert_true(found == array + 10);
 }
 
 static void test_should_not_find_any_when_some_equal_but_not_adjacent(void) {
     nstl_int array[10] = {0, 99, 2, 3, 99, 5, 6, 99, 8, 99};
     nstl_pint found = adjacent_find(array, array + 10);
+    nstl_pint found_cmp = adjacent_find_cmp(array, array + 10);
+    assert_true(found == found_cmp);
     assert_true(found == array + 10);
 }
 
 static void test_should_find_first_when_equal_and_adjacent(void) {
     nstl_int array[10] = {0, 1, 2, 2, 4, 5, 6, 7, 8, 9};
     nstl_pint found = adjacent_find(array, array + 10);
+    nstl_pint found_cmp = adjacent_find_cmp(array, array + 10);
+    assert_true(found == found_cmp);
     assert_true(found == &array[2]);
 }
 
 static void test_should_find_first_of_all_when_many_equal_and_adjacent(void) {
     nstl_int array[10] = {0, 1, 2, 2, 4, 4, 6, 7, 8, 9};
     nstl_pint found = adjacent_find(array, array + 10);
+    nstl_pint found_cmp = adjacent_find_cmp(array, array + 10);
+    assert_true(found == found_cmp);
     assert_true(found == &array[2]);
 }
 
+#undef adjacent_find_cmp
 #undef adjacent_find
 
 extern void test_fixture_adjacent_find(void) {
