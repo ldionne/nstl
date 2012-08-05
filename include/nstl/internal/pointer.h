6dec6bbbd9bc9af2656a047c3bd1fc77f0d0f603
Louis Dionne <louis.dionne92@gmail.com>
2012-08-05 12:35:28 -0400

    Add a dereference proxy to the pointer.

--- include/nstl/internal/pointer.h
+++ include/nstl/internal/pointer.h
@@ -13,6 +13,12 @@
 #define NSTL_POINTER(ValueType, Pointer)                                       \
 NSTL_TYPE(Pointer,                                                             \
                                                                                \
+(defun deref_proxy                                                             \
+NSTL_INSTANTIATE(                                                              \
+    NSTL_I_POINTER_DEREF_PROXY(Pointer, ValueType)                             \
+)                                                                              \
+)                                                                              \
+                                                                               \
 (inherit NSTL_INITIALIZATION_OPERATORS(Pointer))                               \
 (inherit NSTL_ARITHMETIC_OPERATORS(Pointer))                                   \
 (inherit NSTL_COMPARISON_OPERATORS(Pointer))                                   \
@@ -59,11 +65,65 @@ static NSTL_INLINE ValueType nstl_deref(Pointer)(Pointer self) {               \
 )                                                                              \
 /**/
 
+
+#define NSTL_I_POINTER_DEREF_PROXY(Pointer, ValueType)                         \
+NSTL_TYPE(nstl_deref_proxy(Pointer),                                           \
+                                                                               \
+(defstruct                                                                     \
+struct nstl_deref_proxy(Pointer) {                                             \
+    Pointer p;                                                                 \
+};                                                                             \
+typedef struct nstl_deref_proxy(Pointer) nstl_deref_proxy(Pointer);            \
+)                                                                              \
+                                                                               \
+(defun ctor                                                                    \
+static NSTL_INLINE void nstl_ctor(nstl_deref_proxy(Pointer))                   \
+                            (nstl_deref_proxy(Pointer) *proxy, Pointer p) {    \
+    proxy->p = p;                                                              \
+}                                                                              \
+)                                                                              \
+                                                                               \
+(defun dtor                                                                    \
+static NSTL_INLINE void nstl_dtor(nstl_deref_proxy(Pointer))                   \
+                                        (nstl_deref_proxy(Pointer) *proxy) {   \
+    NSTL_SILENCE_UNUSED_VARIABLE_WARNING(proxy);                               \
+}                                                                              \
+)                                                                              \
+                                                                               \
+(defun put                                                                     \
+static NSTL_INLINE void nstl_put(nstl_deref_proxy(Pointer))                    \
+                            (nstl_deref_proxy(Pointer) proxy, ValueType x) {   \
+    nstl_asg(ValueType, ValueType)(proxy.p, x);                                \
+}                                                                              \
+)                                                                              \
+                                                                               \
+(defun get                                                                     \
+static NSTL_INLINE ValueType nstl_get(nstl_deref_proxy(Pointer))               \
+                                        (nstl_deref_proxy(Pointer) proxy) {    \
+    return *proxy.p;                                                           \
+}                                                                              \
+)                                                                              \
+                                                                               \
+(defun get_lvalue                                                              \
+static NSTL_INLINE ValueType *nstl_get_lvalue(nstl_deref_proxy(Pointer))       \
+                                        (nstl_deref_proxy(Pointer) proxy) {    \
+    return proxy.p;                                                            \
+}                                                                              \
+)                                                                              \
+                                                                               \
+)                                                                              \
+/**/
+
+
 /* [[[cog
 
 import nstl
 nstl.generate(cog,
     'deref(Pointer)',
+    'deref_proxy(Iterator)',
+    'put(Proxy)',
+    'get(Proxy)',
+    'get_lvalue(Proxy)',
 
     token=True, mangle=True,
 )
@@ -72,6 +132,14 @@ nstl.generate(cog,
 #include <joy/cat.h>
 #define NSTL_TOKEN_deref (d e r e f)
 #define nstl_deref(Pointer) JOY_CAT3(nstl_mangled_deref, _, Pointer)
+#define NSTL_TOKEN_deref_proxy (d e r e f _ p r o x y)
+#define nstl_deref_proxy(Iterator) JOY_CAT3(nstl_mangled_deref_proxy, _, Iterator)
+#define NSTL_TOKEN_put (p u t)
+#define nstl_put(Proxy) JOY_CAT3(nstl_mangled_put, _, Proxy)
+#define NSTL_TOKEN_get (g e t)
+#define nstl_get(Proxy) JOY_CAT3(nstl_mangled_get, _, Proxy)
+#define NSTL_TOKEN_get_lvalue (g e t _ l v a l u e)
+#define nstl_get_lvalue(Proxy) JOY_CAT3(nstl_mangled_get_lvalue, _, Proxy)
 /* [[[end]]] */
 
 #endif /* !NSTL_INTERNAL_POINTER_H */
