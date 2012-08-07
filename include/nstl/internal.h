dd6f2dab8d052be1f39dbf8c439371079ddea99f
Louis Dionne <louis.dionne92@gmail.com>
2012-08-07 07:23:28 -0400

    Add a wrapper over the standard library.
    I do not know whether it is useful to use a wrapper, so I'm not taking
    the chance.

--- include/nstl/internal.h
+++ include/nstl/internal.h
@@ -14,6 +14,7 @@
 #include <nstl/internal/primitive.h>
 #include <nstl/internal/static_assert.h>
 #include <nstl/internal/static_eval.h>
+#include <nstl/internal/stdlib_wrapper.h>
 #include <nstl/internal/token.h>
 #include <nstl/internal/type.h>
 #include <nstl/internal/workaround.h>
---
+++ include/nstl/internal/stdlib_wrapper.h
@@ -0,0 +1,13 @@
+/**
+ * Wrapper over the standard C library.
+ */
+
+#ifndef NSTL_INTERNAL_STDLIB_WRAPPER_H
+#define NSTL_INTERNAL_STDLIB_WRAPPER_H
+
+#include <string.h>
+
+
+#define nstl_memmove memmove
+
+#endif /* !NSTL_INTERNAL_STDLIB_WRAPPER_H */
