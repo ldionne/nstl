6b87713a93fb98948e7ef547472debef3ea1aab6
Louis Dionne <louis.dionne92@gmail.com>
2012-08-12 11:08:43 -0400

    Add the find_comp algorithm.

--- include/nstl/algorithm/find.h
+++ include/nstl/algorithm/find.h
@@ -1,5 +1,5 @@
 /**
- * This file defines the @em find algorithm.
+ * This file defines the @em find and @em find_comp algorithms.
  */
 
 #ifndef NSTL_ALGORITHM_FIND_H
@@ -20,22 +20,55 @@
 NSTL_TYPE(algo,                                                                \
                                                                                \
 (defun find                                                                    \
-static NSTL_INLINE T nstl_helper(algo, deref)(Iter it_) {                      \
-    nstl_deref_proxy(Iter) proxy;                                              \
-    T result;                                                                  \
-    nstl_ctor(nstl_deref_proxy(Iter))(&proxy, it_);                            \
-    result = nstl_get(nstl_deref_proxy(Iter))(proxy);                          \
-    nstl_dtor(nstl_deref_proxy(Iter))(&proxy);                                 \
-    return result;                                                             \
-}                                                                              \
+typedef nstl_bool (*nstl_helper(algo, impl_pred))(T, T);                       \
+NSTL_GETF(                                                                     \
+    NSTL_I_FIND_COMP(                                                          \
+        nstl_helper(algo, impl),                                               \
+        Iter,                                                                  \
+        T,                                                                     \
+        nstl_helper(algo, impl_pred)                                           \
+    ),                                                                         \
+    find_comp                                                                  \
+)                                                                              \
                                                                                \
-static NSTL_INLINE Iter algo(Iter first_, Iter last_, T value_) {              \
+static NSTL_INLINE Iter algo(Iter first, Iter last, T value) {                 \
+    return nstl_helper(algo, impl)(first, last, value, nstl_eq(T, T));         \
+}                                                                              \
+)                                                                              \
+                                                                               \
+)                                                                              \
+/**/
+
+
+#define NSTL_FIND_COMP(SinglePassReadableIterator, T, Compare)                 \
+    NSTL_I_FIND_COMP(                                                          \
+        nstl_find_comp(SinglePassReadableIterator, Compare),                   \
+        SinglePassReadableIterator,                                            \
+        T,                                                                     \
+        Compare                                                                \
+    )                                                                          \
+/**/
+
+#define NSTL_I_FIND_COMP(algo, Iter, T, Comp)                                  \
+NSTL_TYPE(algo,                                                                \
+                                                                               \
+(defun find_comp                                                               \
+static NSTL_INLINE Iter algo(Iter first_, Iter last_, T value_, Comp comp_) {  \
     Iter first;                                                                \
     nstl_copy_ctor(Iter)(&first, first_);                                      \
                                                                                \
-    while (nstl_ne(Iter, Iter)(first, last_) &&                                \
-           nstl_ne(T, T)(nstl_helper(algo, deref)(first), value_))             \
-        nstl_inc(Iter)(&first);                                                \
+    for ( ; nstl_ne(Iter, Iter)(first, last_); nstl_inc(Iter)(&first)) {       \
+        nstl_bool is_eq;                                                       \
+        {                                                                      \
+            nstl_deref_proxy(Iter) proxy;                                      \
+            nstl_ctor(nstl_deref_proxy(Iter))(&proxy, first);                  \
+            is_eq = comp_(nstl_get(nstl_deref_proxy(Iter))(proxy), value_);    \
+            nstl_dtor(nstl_deref_proxy(Iter)(&proxy));                         \
+        }                                                                      \
+        if (is_eq)                                                             \
+            break;                                                             \
+    }                                                                          \
+                                                                               \
     return first;                                                              \
 }                                                                              \
 )                                                                              \
@@ -49,6 +82,7 @@ static NSTL_INLINE Iter algo(Iter first_, Iter last_, T value_) {              \
 import nstl
 nstl.generate(cog,
     'find(SinglePassReadableIterator)',
+    'find_comp(SinglePassReadableIterator, Compare)',
 
     token=True, mangle=True,
 )
@@ -57,6 +91,8 @@ nstl.generate(cog,
 #include <joy/cat.h>
 #define NSTL_TOKEN_find (f i n d)
 #define nstl_find(SinglePassReadableIterator) JOY_CAT3(nstl_mangled_find, _, SinglePassReadableIterator)
+#define NSTL_TOKEN_find_comp (f i n d _ c o m p)
+#define nstl_find_comp(SinglePassReadableIterator,  Compare) JOY_CAT5(nstl_mangled_find_comp, _, SinglePassReadableIterator, _,  Compare)
 /* [[[end]]] */
 
 #endif /* !NSTL_ALGORITHM_FIND_H */
--- test/algorithm/find.c
+++ test/algorithm/find.c
@@ -1,5 +1,5 @@
 /**
- * Unit tests for the @em find algorithm.
+ * Unit tests for the @em find and @em find_comp algorithms.
  */
 
 #include <nstl/algorithm/find.h>
@@ -7,27 +7,43 @@
 #include <seatest.h>
 
 
+typedef nstl_bool (*Compare)(nstl_int x, nstl_int y);
 NSTL_INSTANTIATE(NSTL_FIND(nstl_pint, nstl_int))
+NSTL_INSTANTIATE(NSTL_FIND_COMP(nstl_pint, nstl_int, Compare))
+
+#define find nstl_find(nstl_pint)
+#define find_comp(first, last, value)                                          \
+    nstl_find_comp(nstl_pint, Compare)(                                        \
+        first, last, value, nstl_eq(nstl_int, nstl_int)                        \
+    )                                                                          \
+/**/
 
 static void test_should_return_last_when_not_found(void) {
     nstl_int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    nstl_pint first = array, last = array + 10, iter;
+    nstl_pint first = array, last = array + 10, iter, iter_comp;
 
-    iter = nstl_find(nstl_pint)(first, last, -1);
+    iter = find(first, last, -1);
+    iter_comp = find_comp(first, last, -1);
+    assert_true(iter == iter_comp);
     assert_true(iter == last);
 }
 
 static void test_should_return_iterator_to_found_element_when_found(void) {
     nstl_int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     nstl_size_t i;
-    nstl_pint first = array, last = array + 10, iter;
+    nstl_pint first = array, last = array + 10, iter, iter_comp;
 
     for (i = 0; i < 10; ++i) {
-        iter = nstl_find(nstl_pint)(first, last, i);
+        iter = find(first, last, i);
+        iter_comp = find_comp(first, last, i);
+        assert_true(iter == iter_comp);
         assert_true(iter == first + i);
         assert_int_equal(*iter, i);
     }
 }
+
+#undef find_comp
+#undef find
 
 extern void test_fixture_find(void) {
     test_fixture_start();
